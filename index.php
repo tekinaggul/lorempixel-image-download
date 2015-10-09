83aa601315926fc56cbeb4771017dbf52aa64b8c
Tekin Ağgül <tekinaggul@gmail.com>
2015-10-09 14:17:05 +0300

    Folder create

--- index.php
+++ index.php
@@ -9,21 +9,23 @@
 <div id="post"></div>
 <script type="text/javascript">
 	var cat = ["abstract","animals","business","cats","city","food","nightlife","fashion","people","nature","sports","technics","transport"];
-	
+	var size = [["600","600"],["400","400"],["400","200"]]
 	$("#gonder").click(function(event) {
-		for (var j = cat.length - 1; j >= 0; j--) {
-			for (var i = 1; i <= 10; i++) {
-				$.ajax({
-				    url: 'post.php',
-				    type: 'POST',
-				    data: {
-				        address: 'http://lorempixel.com/1920/1920/'+cat[j]+'/'+i, name:cat[j]+"-"+i
-				    },
-				    dataType: "script",
-				    success: function(response) {
-				       	$("#post").html(response);
-				    }
-				});
+		for (var k = size.length - 1; k >= 0; k--) {
+			for (var j = cat.length - 1; j >= 0; j--) {
+				for (var i = 1; i <= 10; i++) {
+					$.ajax({
+					    url: 'post.php',
+					    type: 'POST',
+					    data: {
+					        address: 'http://lorempixel.com/'+size[k][0]+'/'+size[k][1]+'/'+cat[j]+'/'+i, name:i,path_name:cat[j],sizeH:size[k][0],sizeV:size[k][1]
+					    },
+					    dataType: "script",
+					    success: function(response) {
+					       	$("#post").html(response);
+					    }
+					});
+				};
 			};
 		};
 	});
